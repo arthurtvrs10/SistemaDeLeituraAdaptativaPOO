6acb23da9045365d22bbe66a1265a7e3f02226d0
tavares <arthurtavares0222@gmail.com>
2026-06-03 20:04:30 -0300

    refac: cria o DocumentService

--- mvc/src/main/java/com/backend/mvc/model/Document.java
+++ mvc/src/main/java/com/backend/mvc/model/Document.java
@@ -1,20 +1,21 @@
 package com.backend.mvc.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import javax.print.attribute.standard.DateTimeAtCreation;
-import java.util.UUID;
 
 @Entity
 public class Document {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id; // é salvo como um VARCHAR(36) OU BINARY(16)
+    private long id; // é salvo como um VARCHAR(36) OU BINARY(16)
                     // "123e4567-e89b-12d3-a456-426614174000"  -
+
+    private String originalFileName;
 
     private String title; // representa o nome original do arquivo PDF
 
@@ -22,5 +23,40 @@ public class Document {
 
     private int totalPages; // total de paginas do PDF
 
-    private DateTimeAtCreation uploadedAt; // deve guardar data e hora do upload
+    private LocalDateTime uploadedAt; // deve guardar data e hora do upload
+
+    public Document(){
+    }
+
+    public Document(String title, String originalFileName, String filePath, Integer totalPages) {
+        this.title = title;
+        this.originalFileName = originalFileName;
+        this.filePath = filePath;
+        this.totalPages = totalPages;
+        this.uploadedAt = LocalDateTime.now();
+    }
+
+     public long getId() {
+        return id;
+    }
+
+    public String getOriginalFileName() {
+        return originalFileName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
 }
--- mvc/src/main/java/com/backend/mvc/repository/DocumentRepository.java
+++ mvc/src/main/java/com/backend/mvc/repository/DocumentRepository.java
@@ -3,7 +3,7 @@ package com.backend.mvc.repository;
 import com.backend.mvc.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+// import java.util.UUID;
 
-public interface DocumentRepository extends JpaRepository<Document, UUID> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
